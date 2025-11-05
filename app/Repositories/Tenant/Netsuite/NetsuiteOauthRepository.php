0d3cb08b35aa2a91fe8348bcb4deb0657245f3dd
Devin Eden <devin.ede@gabbwireless.com>
2025-11-04 22:09:20 -0700

    switching out cache storage

--- app/Repositories/Tenant/Netsuite/NetsuiteOauthRepository.php
+++ app/Repositories/Tenant/Netsuite/NetsuiteOauthRepository.php
@@ -8,8 +8,10 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\Client\RequestException;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class NetsuiteOauthRepository extends BaseNetsuiteRepository
@@ -21,19 +23,22 @@ class NetsuiteOauthRepository extends BaseNetsuiteRepository
     public function generateRedirect() {
         $this->loadConfig();
         $code_verifier = Str::random(128);
+
+        Storage::put('app/cache-verify.txt', $code_verifier);
+        $state = Str::random(45);
+        Storage::put('app/cache-state.txt', $state);
         $code_challenge = strtr(rtrim(base64_encode(hash('sha256', $code_verifier, true)), '='), '+/', '-_');
         $query_params = [
             'response_type' => 'code',
             'client_id' => $this->config['client_id'],
-            'redirect_uri' => 'https://byu.api.tenants.801saas.com/manage/netsuite/initiate',
+            'redirect_uri' => 'https://byu.api.tenants.801saas.com/setup/netsuite/callback',
             'scope' => 'restlets',
-            'state' => Str::random(45),
+            'state' => $state,
             'code_challenge' => $code_challenge,
             'code_challenge_method' => 'S256'
         ];
 
         $authorizationUrl = "https://{$this->config['netsuite_account']}.app.netsuite.com/app/login/oauth2/authorize.nl?".http_build_query($query_params);
-        session(['oauth2state' => $query_params['state'], 'code_verifier'=>$code_verifier]);
         return $authorizationUrl;
     }
 
@@ -43,20 +48,24 @@ class NetsuiteOauthRepository extends BaseNetsuiteRepository
      */
     public function oAuthSetup(array $data) {
         $this->loadConfig();
-//        if (empty($data['state']) || ($data['state'] !== session('oauth2state'))) {
-//            // Handle invalid state
-//            throw new \Exception("Invalid state");
-//        }
+        $state = Storage::get('app/cache-state.txt');
+
+        if (empty($data['state']) || ($data['state'] !== $state)) {
+            // Handle invalid state
+            throw new \Exception("Invalid state");
+        }
+
+        $verifier = Storage::get('app/cache-verify.txt');
 
         try {
             $payload = [
                 'code' => $data['code'],
-                'redirect_uri' => 'https://byu.api.tenants.801saas.com/manage/netsuite/initiate',
+                'redirect_uri' => 'https://byu.api.tenants.801saas.com/setup/netsuite/callback',
                 'grant_type' => 'authorization_code',
-                'code_verifier' => session('code_verifier')
+                'code_verifier' => $verifier
             ];
             $basic = base64_encode($this->config['client_id'].":".$this->config['client_secret']);
-            $resp = Http::withHeaders(['Authorization'=> "Basic {$basic}"])
+            $resp = Http::withHeaders(['Authorization'=> "Basic {$basic}", 'Content-Type'=> 'application/x-www-form-urlencoded'])
                 ->post("https://{$this->config['netsuite_account']}.suitetalk.api.netsuite.com/services/rest/auth/oauth2/v1/token", $payload)
                 ->throw()
                 ->json();
